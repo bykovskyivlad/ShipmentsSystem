5b593646d1194ad341246c61f7301a962d41a21f
Vladyslav <bikovskijvladislav@gmail.com>
2026-01-18 21:29:46 +0100

    Add Database , first migration

---
+++ Shipments.Api/Data/ShipmentsDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shipments.Shared.Domain.Entities;
+namespace Shipments.Api.Data
+{
+    public class ShipmentsDbContext : DbContext
+    {
+        public ShipmentsDbContext(DbContextOptions<ShipmentsDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Shipment> Shipments => Set<Shipment>();
+        public DbSet<ShipmentEvent> ShipmentEvents => Set<ShipmentEvent>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Shipment>(b =>
+            {
+                b.HasKey(x => x.Id);
+
+                b.Property(x => x.Status)
+                    .HasConversion<int>();
+
+                b.HasMany(x => x.Events)
+                    .WithOne(x => x.Shipment!)
+                    .HasForeignKey(x => x.ShipmentId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<ShipmentEvent>(b =>
+            {
+                b.HasKey(x => x.Id);
+
+                b.Property(x => x.OldStatus).HasConversion<int?>();
+                b.Property(x => x.NewStatus).HasConversion<int?>();
+            });
+        }
+    }
+}

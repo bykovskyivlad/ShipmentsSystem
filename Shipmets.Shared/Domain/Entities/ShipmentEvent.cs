190fc6f5c862dec8602626c143db6a9565be11ec
Vladyslav <bikovskijvladislav@gmail.com>
2026-01-18 17:15:01 +0100

    Add a Rule and fixed required

---
+++ Shipmets.Shared/Domain/Entities/ShipmentEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shipments.Shared.Domain.Enums;
+
+namespace Shipments.Shared.Domain.Entities
+{
+    public class ShipmentEvent
+    {
+        public int Id { get; set; }
+        public int ShipmentId { get; set; }
+        public Shipment? Shipment { get; set; }
+        public ShipmentStatus? OldStatus { get; set; }
+        public ShipmentStatus? NewStatus { get; set; }
+        [Required, MaxLength(70)]
+        public string PerformedByUserId { get; set; } = string.Empty;
+        [Required, MaxLength(30)]
+        public string PerformedByRole { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string? Notes { get; set; }
+        public DateTimeOffset OccurredAtUtc { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
---
+++ Shipmets.Shared/Domain/Rules/ShipmentStatusTransitions.cs
@@ -0,0 +1,30 @@
+﻿using Shipments.Shared.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipments.Shared.Domain.Rules
+{
+    public static class ShipmentStatusTransitions
+    {
+        private static readonly Dictionary<ShipmentStatus, HashSet<ShipmentStatus>> Allowed = new()
+        {
+            [ShipmentStatus.Created] = new HashSet<ShipmentStatus> { ShipmentStatus.PickedUp, ShipmentStatus.Canceled },
+            [ShipmentStatus.PickedUp] = new HashSet<ShipmentStatus> { ShipmentStatus.OutForDelivery },
+            [ShipmentStatus.OutForDelivery] = new HashSet<ShipmentStatus> { ShipmentStatus.Delivered, ShipmentStatus.DeliveryFailed },
+            [ShipmentStatus.Delivered] = new HashSet<ShipmentStatus>(),
+            [ShipmentStatus.Canceled] = new HashSet<ShipmentStatus>()
+        };
+        /// <summary>
+        /// Sprawdza, czy przejście ze statusu źródłowego do docelowego jest dozwolone.
+        /// </summary>
+        public static bool CanTransition(ShipmentStatus from, ShipmentStatus to) => Allowed.TryGetValue(from, out var targets) && targets.Contains(to);
+        /// <summary>
+        /// Zwraca listę dozwolonych statusów docelowych dla danego statusu źródłowego.
+        /// </summary>
+        public static IReadOnlyCollection<ShipmentStatus> GetAllowedTransitions(ShipmentStatus from) => Allowed.TryGetValue(from, out var targets) ? targets.ToArray() : Array.Empty<ShipmentStatus>();
+
+    }
+}

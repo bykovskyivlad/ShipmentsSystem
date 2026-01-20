946cd92dbf162a5b1c547b5b1ea444c3886a655d
Oleh Dyrda <odyrda841@gmail.com>
2026-01-20 19:26:48 +0100

    Adding Services for conecting api to MVC and storing JWT tokens

---
+++ Shipments.Mvc/Services/TokenCookieService.cs.cs
@@ -0,0 +1,27 @@
+﻿namespace Shipments.Mvc.Services;
+
+public interface ITokenCookieService
+{
+    void SetToken(HttpResponse response, string token);
+    string? GetToken(HttpRequest request);
+    void ClearToken(HttpResponse response);
+}
+
+public class TokenCookieService : ITokenCookieService
+{
+    private const string CookieName = "shipments_jwt";
+
+    public void SetToken(HttpResponse response, string token)
+        => response.Cookies.Append(CookieName, token, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Lax
+        });
+
+    public string? GetToken(HttpRequest request)
+        => request.Cookies.TryGetValue(CookieName, out var token) ? token : null;
+
+    public void ClearToken(HttpResponse response)
+        => response.Cookies.Delete(CookieName);
+}
